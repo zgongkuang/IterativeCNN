12dc6c21645db2886f7bb972387f5c51f0b0a3d5
Kuang Gong <kgong@mgh.harvard.edu>
2018-09-14 14:34:12 -0400

    Update dump.m

--- utils/dump.m
+++ utils/dump.m
@@ -8,18 +8,3 @@ end
 fid = fopen(filename, 'wb');
 fwrite(fid, data, type);
 fclose(fid);
-% 
-% 
-% clear;
-% clc;
-% close all
-% fid = fopen('all_direct_geoCombine79.data','wb');
-% for filenum = 1:4000
-%     filenum
-%     str = sprintf('Direct_plane79_%d.mat',filenum);
-%     load (str);
-%     fwrite(fid, yy_Measure, 'float32');
-%     clear yy_Measure;
-% end
-% fclose(fid);    
-    
